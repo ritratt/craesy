25ce0d4c15fba3f44af9552464180929031ae64f
Rituraj <ritratt@Clux.(none)>
2012-08-01 00:59:37 +0530

    Update /bin/craesy, /scripts/EncryptDecrypt.py, /scripts/GetFiles.py Added feature: Recursive encryption, Encrypt/Decrypt does not require destination filename.
    Details:
    Modified craesy,ED,Getfiles to support recursive encryption.
    Destintation file name is source file name by default. Destination os encrypted file extension is .csy (short for craesy) as default.
    
    To Do:
    Recursive decryption
    Support for regular expressions for filenames.

--- scripts/EncryptDecrypt.py
+++ scripts/EncryptDecrypt.py
@@ -2,6 +2,7 @@ from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
 from pbkdf2 import PBKDF2
 import getpass,sys
+import GetFiles
 
 class ED(object):
   def getfromuser(self,*args):
@@ -15,26 +16,30 @@ class ED(object):
     if choice=='key':
         password=getpass.getpass('Enter AES your password: \n>')
         key=PBKDF2(password,IV).read(32)
-        print key
         return key
             
-  def AESEncrypt(self,key,IV,source,dest):
+  def AESEncrypt(self,key,IV,source):
+    files=GetFiles.getfiles(source)
+    for a_file in files:
+    	f=open(a_file,"r")
+        fstream=f.read()
+        f.close()
+        source_ext=source[-3:]
+        AES_stream=AES.new(key,AES.MODE_CFB,IV)
+        AES_encrypted=AES_stream.encrypt(fstream)
+        dest=str(a_file)[:-2]+'csy'
+        with open(dest,"w") as write_file:
+        	write_file.write(str(IV)+source_ext+AES_encrypted)
+        
+        
+  def AESDecrypt(self,key,IV,source):
     f=open(source,"r")
     fstream=f.read()
     f.close()
-    
+    dest_ext=fstream[16:19]
+    print dest_ext
+    dest=source[:-3]+dest_ext
     AES_stream=AES.new(key,AES.MODE_CFB,IV)
-    AES_encrypted=AES_stream.encrypt(fstream)
-    
-    with open(dest,"w") as write_file:
-        write_file.write(str(IV)+AES_encrypted)
-        
-        
-  def AESDecrypt(self,key,IV,source,dest):
-    f=open(source,"r")
-    fstream=f.read()[16:]
-    f.close()
-    AES_stream=AES.new(key,AES.MODE_CFB,IV)
-    AES_decrypted=AES_stream.decrypt(fstream)
+    AES_decrypted=AES_stream.decrypt(fstream[19:])
     with open(dest,"w") as write_file:
         write_file.write(AES_decrypted)
--- scripts/GetFiles.py
+++ scripts/GetFiles.py
@@ -1,12 +1,24 @@
-import os,itertools
+import os,itertools,sys
 
-class GetFiles(object):
-	def __init__(self,userpath):
-		print userpath
-		filepath=[]
-		contents=os.walk(userpath)
-		for folder in contents:
-			for fileonly in folder[2]:
-				filepath.append(str(folder[0])+'/'+str(fileonly))
-		return filepath
+def getfiles(userpath):
+	filepath=[]
+	contents=os.walk(userpath)
+	temp = contents
+	temp_list=list(temp)
+	if len(temp_list)==0:	#This means that either the path points to a single file or a non-existent file/folder.
+		try:
+			with open(userpath) as f:	
+				pass
+			return userpath.split()	#Applied split function to convert the string to a list.
+		except IOError:
+			print 'Invalid path.'
+			sys.exit()
+	for folder in contents:
+		for fileonly in folder[2]:
+			filepath(append(str(folder[0])+'/'+str(fileonly)))
+	if (len(filepath)==0):
+		print 'No files to encrypt in the given folder.'
+		sys.exit()
+	return filepath
+
 
