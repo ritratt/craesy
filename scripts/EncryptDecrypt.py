309f1da853dedb7b74748add83fddcb0e32c1e04
Rituraj <ritratt@Clux.(none)>
2012-08-02 01:25:12 +0530

    Update: craesy,ED,Getfiles Fix:Recursive Encryption & Decryption, IV generation
    Committer: Rituraj
    On branch master
    Changes to be committed:
    use "git reset HEAD <file>..." to unstage)
    modified:   bin/craesy
    modified:   scripts/EncryptDecrypt.py
    modified:   scripts/GetFiles.py
    
    Changes:
    Recursive encryption decryption fixed so that all files in a folder and subfolder can be E/D using a single password.
    IV generation fixed. Each encrypted file has a different IV from which the key is generated and the file encrypted.
    
    To-Do:
    1. Regular Expressions for files to E/D.
    2. Option for partial recurice E/D.
    3. Option to display which files are going to be accessed.

--- scripts/EncryptDecrypt.py
+++ scripts/EncryptDecrypt.py
@@ -1,45 +1,54 @@
 from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
 from pbkdf2 import PBKDF2
-import getpass,sys
+import getpass,sys,random
 import GetFiles
 
 class ED(object):
-  def getfromuser(self,*args):
-    try:
-        choice=args[0]
-        IV=args[1]
-    except IndexError:
-        print 'Error processing IV.'
-        sys.exit()
+  	def generatekey(self,password,IV):
+   # try:
+    #    choice=args[0]
+     #   IV=args[1]
+   # except IndexError:
+    #    print 'Error processing IV.'
+     #   sys.exit()
         
-    if choice=='key':
-        password=getpass.getpass('Enter AES your password: \n>')
-        key=PBKDF2(password,IV).read(32)
-        return key
-            
-  def AESEncrypt(self,key,IV,source):
-    files=GetFiles.getfiles(source)
-    for a_file in files:
-    	f=open(a_file,"r")
-        fstream=f.read()
-        f.close()
-        source_ext=source[-3:]
-        AES_stream=AES.new(key,AES.MODE_CFB,IV)
-        AES_encrypted=AES_stream.encrypt(fstream)
-        dest=str(a_file)[:-2]+'csy'
-        with open(dest,"w") as write_file:
-        	write_file.write(str(IV)+source_ext+AES_encrypted)
+   # if choice=='key':
+		key=PBKDF2(password,IV).read(32)
+        	return key
+	def getfromuser(self,choice):
+		if choice=='password':
+	        	password=getpass.getpass('Enter AES your password: \n>')
+			return password
+	def AESEncrypt(self,password,source):
+		files=GetFiles.getfiles(source)
+		for a_file in files:
+    			f=open(a_file,"r")
+        		fstream=f.read()
+        		f.close()
+			IV=SHA256.new(str(random.randint(0,2**100))).hexdigest()[0:16]
+			key=self.generatekey(password,IV)
+        		source_ext=a_file[-3:]
+        		AES_stream=AES.new(key,AES.MODE_CFB,IV)
+        		AES_encrypted=AES_stream.encrypt(fstream)
+        		dest=str(a_file)[:-3]+'csy'
+        		with open(dest,"w") as write_file:
+				write_file.write(str(IV)+source_ext+AES_encrypted)
+		print 'Encryption done!'
         
         
-  def AESDecrypt(self,key,IV,source):
-    f=open(source,"r")
-    fstream=f.read()
-    f.close()
-    dest_ext=fstream[16:19]
-    print dest_ext
-    dest=source[:-3]+dest_ext
-    AES_stream=AES.new(key,AES.MODE_CFB,IV)
-    AES_decrypted=AES_stream.decrypt(fstream[19:])
-    with open(dest,"w") as write_file:
-        write_file.write(AES_decrypted)
+	def AESDecrypt(self,password,source):
+		files=GetFiles.getfiles(source)
+    		for a_file in files:
+			f=open(a_file,"r")
+			fstream=f.read()
+			f.close()
+			IV=fstream[0:16]
+			key=self.generatekey(password,IV)
+			dest_ext=fstream[16:19]
+			dest=a_file[:-3]+dest_ext
+			AES_stream=AES.new(key,AES.MODE_CFB,IV)
+			AES_decrypted=AES_stream.decrypt(fstream[19:])
+			with open(dest,"w") as write_file:
+				write_file.write(AES_decrypted)
+		print 'Decryption done!'
--- scripts/GetFiles.py
+++ scripts/GetFiles.py
@@ -5,17 +5,22 @@ def getfiles(userpath):
 	contents=os.walk(userpath)
 	temp = contents
 	temp_list=list(temp)
+	#print temp_list
 	if len(temp_list)==0:	#This means that either the path points to a single file or a non-existent file/folder.
 		try:
+	#		print userpath
 			with open(userpath) as f:	
-				pass
+				print 'File found.'
 			return userpath.split()	#Applied split function to convert the string to a list.
 		except IOError:
 			print 'Invalid path.'
 			sys.exit()
+
+	contents=os.walk(userpath)
 	for folder in contents:
 		for fileonly in folder[2]:
-			filepath(append(str(folder[0])+'/'+str(fileonly)))
+			filepath.append(str(folder[0])+'/'+str(fileonly))
+	#		print filepath
 	if (len(filepath)==0):
 		print 'No files to encrypt in the given folder.'
 		sys.exit()
